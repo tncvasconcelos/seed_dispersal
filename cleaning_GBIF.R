527b97457783bc18663a13b4a23c8e9b133272fd
tncvasconcelos <thais.nogales@gmail.com>
2021-01-12 15:47:30 -0600

    adding climatic data

--- cleaning_GBIF.R
+++ cleaning_GBIF.R
@@ -5,8 +5,7 @@
 library(ape)
 library(taxize)
 library(rgbif)
-tree.dir <- paste0(getwd(), "/trees")
-trait.dir <- paste0(getwd(), "/trait_data")
+tree.dir <- "~/Desktop/climate_niche_seed_dispersal/seed_dispersal/trees"
 
 # Trees
 tree_files <- list.files(tree.dir, ".tre")
@@ -15,18 +14,21 @@ trees <- lapply(trees, ladderize)
 labels <- unlist(lapply(strsplit(tree_files, "_"), "[[", 1))
 names(trees) <- labels
 
+#length(unlist(lapply(trees, "[[","tip.label")))
+
+
 #-------------------------------
 # Download GBIF data
 #-------------------------------
 
-# fill in gbif.org credentials 
-user <- "" # your gbif.org username 
+# fill in gbif.org credentials
+user <- "" # your gbif.org username
 pwd <- "" # your gbif.org password
-email <- "" # your email 
+email <- "" # your email
 
 for(i in 1:length(trees)){
   taxized_names <- resolveGBIF(trees[[i]]$tip.label) # it should take about 2-3min
-  rgbif::occ_download(rgbif::pred_in("scientificName", taxized_names), 
+  rgbif::occ_download(rgbif::pred_in("scientificName", taxized_names),
                       pred_in("basisOfRecord", 'PRESERVED_SPECIMEN'),
                       pred("hasCoordinate", TRUE),
                       format = "SIMPLE_CSV", user=user,pwd=pwd,email=email)
@@ -51,6 +53,11 @@ points <- lapply(paste0(points.dir, "/", point_files), fread,quote="")
 names(points) <- unlist(lapply(strsplit(point_files, "_"), "[[", 1))
 #setwd(points.dir)
 
+#setwd("~/Desktop/rangers_all_files/rangers")
+#source("/Users/thaisvasconcelos/Desktop/rangers_all_files/rangers/R/GetTraitDistribution.R")
+#source("/Users/thaisvasconcelos/Desktop/rangers_all_files/rangers/R/GetRanges.R")
+#template.map <- readRDS("R/template.map.Rdata")
+
 all_cleaned_points <- list()
 for(i in 1:length(points)) {
   cleaned_points <- points[[i]][-which(points[[i]]$species==""),]
@@ -63,18 +70,15 @@ for(i in 1:length(points)) {
   cleaned_points <- RemoveHerbariaLocalities(cleaned_points, lon="decimalLongitude", lat="decimalLatitude")
   cleaned_points <- RemoveCropsAndInvasive(cleaned_points, species="species")
   cleaned_points <- RemoveZeros(cleaned_points, lon="decimalLongitude", lat="decimalLatitude")
+  species_to_remove <- UnusualDistributions(cleaned_points, lon="decimalLongitude", lat="decimalLatitude")
+  if(length(species_to_remove)>0) {
+    cleaned_points <- cleaned_points[-which(cleaned_points$species%in%species_to_remove),]
+  }
   all_cleaned_points[[i]] <- cleaned_points
   write.csv(cleaned_points, file=paste0(names(points)[i], "_cleaned.csv"))
 }
 names(all_cleaned_points) <- names(points)
 
-### 
-# to recover cleaned points back:
-points.dir <- "~/Desktop/climate_niche_seed_dispersal/GBIF_points"
-cleaned_point_files <- list.files(points.dir, ".csv")
-cleaned_point_files <- cleaned_point_files[grepl("cleaned", cleaned_point_files)]
-cleaned_points <- lapply(paste0(points.dir, "/", cleaned_point_files), fread)
-names(cleaned_points) <- unlist(lapply(strsplit(cleaned_point_files, "_"), "[[", 1))
 
 # Ploting
 for(family_index in 1:length(all_cleaned_points)) {
@@ -90,15 +94,25 @@ for(family_index in 1:length(all_cleaned_points)) {
     title(spp[species_index])
     print(species_index)
   }
-dev.off()
+  dev.off()
 }
+
+###
+# to recover cleaned points back:
+points.dir <- "~/Desktop/climate_niche_seed_dispersal/GBIF_points"
+cleaned_point_files <- list.files(points.dir, ".csv")
+cleaned_point_files <- cleaned_point_files[grepl("cleaned", cleaned_point_files)]
+cleaned_points <- lapply(paste0(points.dir, "/", cleaned_point_files), fread)
+names(cleaned_points) <- unlist(lapply(strsplit(cleaned_point_files, "_"), "[[", 1))
+
 
 ########
 # Getting climate data
 ########
+
 for(family_index in 1:length(all_cleaned_points)) {
-  # 1. Getting value for every point 
-  allpoints <- DataFromPoints(all_cleaned_points[[family_index]], lon="decimalLongitude", lat="decimalLatitude", res=2.5)
+  # 1. Getting value for every point
+  allpoints <- ClimateFromPoints(all_cleaned_points[[family_index]], lon="decimalLongitude", lat="decimalLatitude", res=2.5)
   write.csv(allpoints, file=paste0(names(all_cleaned_points)[family_index], "_allpoints.csv"))
   # 2. Getting summary statistics of climatic variables for each species
   # Thinning occurence data first
@@ -106,5 +120,76 @@ for(family_index in 1:length(all_cleaned_points)) {
   summstats <- GetClimateSummStats(thinned_points, lat = "lat", lon="lon", res=2.5)
   write.csv(summstats, file=paste0(names(all_cleaned_points)[family_index], "_summstats.csv"))
 }
+
+#-------------------------------
+#-------------------------------
+# Getting organized table for OUwie
+climate_data.dir <- "~/Desktop/climate_niche_seed_dispersal/seed_dispersal/climate_data"
+summstats_files <- list.files(climate_data.dir, "summstats.csv")
+summstats <- lapply(paste0(climate_data.dir, "/", summstats_files), read.csv)
+names(summstats) <- unlist(lapply(strsplit(summstats_files, "_"), "[[", 1))
+
+# Trait data
+trait.dir <- "~/Desktop/climate_niche_seed_dispersal/seed_dispersal/trait_data"
+trait_files <- list.files(trait.dir, ".csv")
+traits <- lapply(paste0(trait.dir, "/", trait_files), read.csv)
+labels <- unlist(lapply(strsplit(trait_files, "_"), "[[", 1))
+names(traits) <- labels
+
+cleaned_table_abiotic <- cleaned_table_biotic <- matrix(nrow=0, ncol=3)
+
+for(group_index in 1:length(labels)) {
+  group <- names(traits)[group_index]
+  group_traits <- traits[[group]][,1:2]
+  group_summstats <- summstats[[grep(group, names(summstats))]]
+
+  # Matching datasets
+  group_summstats$species <- sub(" ","_", group_summstats$species)
+  merged_table <- merge(group_summstats, group_traits, by.x="species", by.y="Species")
+
+  cleaned_table <- merged_table[,c("species","mean_bio1","mean_bio10","mean_bio11","mean_bio12",
+                                   "mean_bio13","mean_bio14","Dispersal_mode")]
+
+  # Creating vectors for temperature and preciptation mean and breadth
+  cleaned_table$temp_breadth <- as.numeric(cleaned_table$mean_bio10) - as.numeric(cleaned_table$mean_bio11)
+  cleaned_table$prec_breadth <- as.numeric(cleaned_table$mean_bio13) - as.numeric(cleaned_table$mean_bio14)
+  # reorganizing
+  cleaned_table <- cleaned_table[,c("species","mean_bio1","temp_breadth","mean_bio12","prec_breadth","Dispersal_mode")]
+  colnames(cleaned_table) <- c("species","temp","temp_breadth","prec","prec_breadth","Dispersal_mode")
+  write.csv(cleaned_table, file=paste0(trait.dir,"/",group, "_niche.csv"))
+}
+
+
+## Maping dispersal modes
+
+#setwd("~/Desktop/rangers_all_files/rangers")
+#source("/Users/thaisvasconcelos/Desktop/rangers_all_files/rangers/R/GetTraitDistribution.R")
+#source("/Users/thaisvasconcelos/Desktop/rangers_all_files/rangers/R/GetRanges.R")
+#template.map <- readRDS("R/template.map.Rdata")
+pal <- hcl.colors(30, palette = "SunsetDark", alpha = 1)
+
+for(i in 1:length(cleaned_points)) {
+  group <- names(traits)[i]
+  traits_group <- traits[[i]][,1:2]
+  points <- cleaned_points[[i]]
+  ranges <- GetRanges(points)
+
+  traits_group[,1] <- sub("_"," ", traits_group[,1])
+  traits_subset <- traits_group[traits_group$Species %in% names(ranges),]
+  ranges_subset <- ranges[names(ranges) %in% traits_subset$Species]
+
+  fruit_distribution <- GetTraitDistribution(ranges_subset, traits_subset, type="binary")
+
+  pdf(paste0(getwd(), "/", group, ".pdf"), height=8, width=6)
+  par(mfrow=c(2,1))
+  plot(template.map, col="gray", legend=F)
+  plot(fruit_distribution$Biotic, add=T, col=rev(pal))
+  title("Biotic")
+  plot(template.map, col="gray", legend=F)
+  plot(fruit_distribution$Abiotic, add=T, col=rev(pal))
+  title("Abiotic")
+  dev.off()
+}
+getwd()
 
 
--- tree_plots.R
+++ tree_plots.R
@@ -1,11 +1,12 @@
 # Checking distribution of seed dispersal mode on trees
 rm(list=ls())
 library(ape)
-setwd("~/Desktop/climate_niche_seed_dispersal/data")
-setwd("~/2021_SeedDispersal/")
+setwd("~/Desktop/climate_niche_seed_dispersal/seed_dispersal")
+#setwd("~/2021_SeedDispersal/")
 
 tree.dir <- paste0(getwd(), "/trees")
 trait.dir <- paste0(getwd(), "/trait_data")
+climate_data.dir <-  paste0(getwd(), "/climate_data")
 
 # Trees
 tree_files <- list.files(tree.dir, ".tre")
@@ -20,12 +21,16 @@ traits <- lapply(paste0(trait.dir, "/", trait_files), read.csv)
 labels <- unlist(lapply(strsplit(trait_files, "_"), "[[", 1))
 names(traits) <- labels
 
-# 
-# plots
+# Climate data
+summstats_files <- list.files(climate_data.dir, "summstats.csv")
+summstats <- lapply(paste0(climate_data.dir, "/", summstats_files), read.csv)
+names(summstats) <- unlist(lapply(strsplit(summstats_files, "_"), "[[", 1))
 
+
+# Tree plots
 for(group_index in 1:length(labels)) {
   group <- names(traits)[group_index]
-  group_traits <- traits[[group]][,1:2] 
+  group_traits <- traits[[group]][,1:2]
   group_tree <- trees[[grep(group, names(trees))]]
 
   to_drop <- c("remove","no_info","doubtful")
@@ -34,13 +39,13 @@ for(group_index in 1:length(labels)) {
   }
   group_traits <- group_traits[group_traits[,1] %in% group_tree$tip.label,]
   group_traits <- group_traits[order(match(group_traits[,1],group_tree$tip.label)),]
-  
+
   mode <- group_traits[,2]
-  names(mode) <- group_traits[,1] 
+  names(mode) <- group_traits[,1]
   colors_states <- c("midnightblue", "goldenrod")
- 
+
   #tip.cols <- colors_states[as.factor(mode)]
- 
+
   pdf(paste0(group, "_dispersal_mode_overview.pdf"), width= 4, height= 12)
   plot(group_tree, show.tip.label=T, edge.width=0.2, adj=1, cex=0.08)
   par(fg="transparent")
@@ -48,10 +53,77 @@ for(group_index in 1:length(labels)) {
   par(fg="black")
   #tiplabels(pch=1, bg=tip.cols, adj=1, cex=0.1, width = 0.1)
 
-  legend("bottomleft", legend=sort(unique(mode)), pt.bg = colors_states, pch=21, cex=0.8)
+  legend("topleft", legend=sort(unique(mode)), pt.bg = colors_states, pch=21, cex=0.8)
   title(main=paste0(group))
   axisPhylo()
   dev.off()
 }
+
+# Tree plots with climatic data
+for(group_index in 1:length(labels)) {
+  # Getting individual data for each group
+  group <- names(traits)[group_index]
+  group_traits <- traits[[group]][,1:2]
+  group_tree <- trees[[grep(group, names(trees))]]
+  group_summstats <- summstats[[grep(group, names(summstats))]]
+
+  # Matching datasets
+  group_summstats$species <- sub(" ","_", group_summstats$species)
+  merged_table <- merge(group_summstats, group_traits, by.x="species", by.y="Species")
+
+  cleaned_table <- merged_table[,c("species","mean_bio1","mean_bio10","mean_bio11","mean_bio12",
+                                  "mean_bio13","mean_bio14","Dispersal_mode")]
+  tree_pruned <- keep.tip(group_tree, cleaned_table$species)
+  #tree_pruned <- ladderize(tree_pruned, F)
+
+  # Organizing so climatic data is in the same order as tips of the tree
+  cleaned_table <- cleaned_table[match(tree_pruned$tip.label, cleaned_table$species),]
+
+  # Creating vectors for temperature and preciptation mean and breadth
+  # The vector has to be in the reverse order to match the tips when ploting
+  temp_breadth <- rev(as.numeric(cleaned_table$mean_bio10) - as.numeric(cleaned_table$mean_bio11))
+  temp_mean <- rev(as.numeric(cleaned_table$mean_bio1))
+  prec_breadth <- rev(as.numeric(cleaned_table$mean_bio13) - as.numeric(cleaned_table$mean_bio14))
+  prec_mean <- rev(as.numeric(cleaned_table$mean_bio12))
+
+  # Setting dispersal mode for plot
+  mode <- rev(cleaned_table$Dispersal_mode)
+  names(mode) <- rev(cleaned_table$species)
+  colors_states <- c("midnightblue", "goldenrod")
+  mode_cols <- mode
+  mode_cols[mode_cols=="Biotic"] <- "goldenrod"
+  mode_cols[mode_cols=="Abiotic"] <- "midnightblue"
+  mode_cols <- mode_cols[match(names(mode_cols),tree_pruned$tip.label)]
+
+  if(group=="Melastomataceae") { # still no idea why Melas doesn't plot right at first
+    mode_cols <- rev(mode_cols)
+  }
+
+  # Plotting
+  pdf(paste0(getwd(), "/plots/", group, "_tree_and_niche.pdf"), height=6, width=4)
+  layout.matrix <- matrix(c(1,2,3), nrow = 1, ncol = 3)
+  layout(mat = layout.matrix,widths = c(2,1,1))
+  par(mar=c(5,0.5,3,0.5))
+
+  # Tree
+  plot(tree_pruned, show.tip.label=F, edge.width=0.2, adj=1, cex=0.08)
+  legend("topleft", legend=sort(unique(mode)), pt.bg = colors_states, pch=21, cex=0.8)
+  title(main=paste0(group))
+  axisPhylo()
+
+  # Temperature
+  x <- 1:length(temp_mean)
+  plot(temp_mean, x, xlim=range(c(temp_mean-temp_breadth, temp_mean+temp_breadth)),
+     pch=19, yaxt = "n", xlab="Temperature (C*100)", ylab="", frame.plot=T, cex=0.3, col=mode_cols)
+  arrows(temp_mean-temp_breadth, x, temp_mean+temp_breadth, x, length=0.05, angle=90, code=3, lwd=0.1, col=mode_cols)
+
+  # Precipitation
+  x <- 1:length(prec_mean)
+  plot(prec_mean, x, xlim=range(c(prec_mean-prec_breadth, prec_mean+prec_breadth)),
+     pch=19, yaxt = "n", xlab="Precipitation (mm)", ylab="", frame.plot=T, cex=0.3,col=mode_cols)
+  arrows(prec_mean-prec_breadth, x, prec_mean+prec_breadth, x, length=0.05, angle=90, code=3, lwd=0.1, col=mode_cols)
+  dev.off()
+}
+
 
 
